b2b9a2aee52c21ee7e426e490013676aa5fde429
mNuraiym <nuraika180903@gmail.com>
2023-02-15 17:02:15 +0600

    dz1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Привет, {this.props.name}</h1>
+  };
+} 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome name ="Sara" />
+      <Welcome name ="Argen" />
+      <Welcome name ="Arsen" />
     </div>
   );
 }
